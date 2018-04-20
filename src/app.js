58c4fe3a88c54b3ba1e6d139d95447c0868b8fb9
Sujan Shrestha <sujanshrestha@Sujans-MacBook-Pro.local>
2018-04-20 23:11:13 +0545

    Remove data Expense data

--- src/app.js
+++ src/app.js
@@ -10,15 +10,6 @@ import './styles/style.scss';
 
 const store = configureStore();
 
-const expenseOne = store.dispatch(addExpense({description : 'Water Bill', amount : 4500}));
-const expenseTwo = store.dispatch(addExpense({description : 'Gas Bill', createdAt : 1000}));
-const expenseThree = store.dispatch(addExpense({description : 'Rent', amount : 109500}));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store = {store}>
     <AppRouter />
