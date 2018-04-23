0a0e633546ad7caee56877b2e20c4e65f60930f2
Sujan Shrestha <sujanshrestha@Sujans-MacBook-Pro.local>
2018-04-24 01:03:13 +0545

    NotFOundPAge

--- src/components/NoPageFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const NoPageFound = () => (
-  <div>
-    <p>404! - <Link to = "/">Go Home</Link></p>
-  </div>
-);
-export default NoPageFound;
