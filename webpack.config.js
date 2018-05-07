915c925601dcba9baa7f8025699058e534f333ff
Sujan Shrestha <sujanshrestha@Sujans-MacBook-Pro.local>
2018-05-07 23:55:34 +0545

    Some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -51,12 +51,12 @@ module.exports = (env) => {
     plugins : [
       CSSExtract,
       new webpack.DefinePlugic({
-        'process.env.FIREBASE_API_KEY' : JSON.strigigy(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN' : JSON.strigigy(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL' : JSON.strigigy(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID' : JSON.strigigy(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET' : JSON.strigigy(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID' : JSON.strigigy(process.env.FIREBASE_MESSAGING_SENDER_ID)
+        'process.env.FIREBASE_API_KEY' : JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN' : JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL' : JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID' : JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET' : JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID' : JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
     ],
     devtool : isProduction ? 'source-map' : 'inline-source-map',
