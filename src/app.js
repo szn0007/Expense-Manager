eb7b1de434817c75e2030adfe58efa63025914b1
Sujan Shrestha <sujanshrestha@Sujans-MacBook-Pro.local>
2018-05-15 23:56:52 +0545

    AUTHENTICATION AND PRIVATE ROUTING

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
+import AppRouter , { history } from './routers/AppRouter';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './action/expenses';
-import {setTextFilter} from './action/filters';
+import {login , logout} from './action/auth';
 import getVisibleExpenses from './selectors/expenses';
 import './styles/style.scss';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
 
@@ -17,8 +17,31 @@ const jsx = (
   </Provider>
 );
 
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
+
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // console.log('Logged In');
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+    renderApp();
+    if(history.location.pathname === '/'){
+      history.push('/dashboard');
+    }
+    });
+  } else {
+    // console.log('Logged Out!');
+    store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { NavLink} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../action/auth';
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
   <header>
     <h1>Expense Manager</h1>
-    <NavLink to = "/" activeClassName = "is-active" exact = {true} >Dashboard</NavLink>
+    <NavLink to = "/dashboard" activeClassName = "is-active" exact = {true} >Dashboard</NavLink>
     <NavLink to = "/create" activeClassName = "is-active">Create Expense</NavLink>
     <NavLink to = "/income" activeClassName = "is-active">Add Income</NavLink>
-    <NavLink to = "/help" activeClassName = "is-active">Help</NavLink>
+    <button onClick = { startLogout }>LogOut</button>
   </header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+  startLogout : () => dispatch(startLogout())
+});
+
+export default connect (undefined, mapDispatchToProps)(Header);
